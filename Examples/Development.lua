50d9efb88f898302c4d75582b324c8ed2dbde4b7
AsynchronousMatrix <catbob614@gmail.com>
2023-07-19 23:00:25 +0100

    feature: update 'Development' script with latest dataset

--- Examples/Development.lua
+++ Examples/Development.lua
@@ -1,8 +1,6 @@
 local DiscordLuaU = require("../Source/init")
 
-local Task = require("@lune/task")
-
-local DISCORD_BOT_TOKEN = "NzI2ODE5MTc5MDU0ODkxMTQ5.GlmKcT.s2AK7wJQPAVjplkqhb0JKfxPALkschibCGLpkQ"
+local DISCORD_BOT_TOKEN = "NzI2ODE5MTc5MDU0ODkxMTQ5.G65VEW.--------------------------------------"
 
 local DiscordClient = DiscordLuaU.DiscordClient.new({
 	token = DISCORD_BOT_TOKEN,
@@ -14,32 +12,21 @@ local DiscordClient = DiscordLuaU.DiscordClient.new({
 -- end)
 
 DiscordClient:Subscribe("OnReady", function()
-	local discordPresence = DiscordLuaU.DiscordPresence.new()
-
 	print(`Bot '{DiscordClient.Username}#{DiscordClient.Discriminator}' is online!`)
 
-	Task.spawn(function()
-		while true do
-			discordPresence:SetStatus(DiscordLuaU.DiscordPresence.Status.Online)
+	local discordPresence = DiscordLuaU.DiscordPresence.new()
+	local discordActivity = DiscordLuaU.DiscordActivity.new()
 
-			DiscordClient:UpdatePresenceAsync(discordPresence):andThen(function()
-				print(
-					`Bot '{DiscordClient.Username}#{DiscordClient.Discriminator}' set status to: {discordPresence.Status}`
-				)
-			end)
+	discordActivity:SetActivityName("I am Testing!")
+	discordActivity:SetActivityType(DiscordLuaU.DiscordActivity.Type.Game)
 
-			Task.wait(20)
+	discordPresence:SetStatus(DiscordLuaU.DiscordPresence.Status.Online)
+	discordPresence:AddActivity(discordActivity)
 
-			discordPresence:SetStatus(DiscordLuaU.DiscordPresence.Status.Idle)
-
-			DiscordClient:UpdatePresenceAsync(discordPresence):andThen(function()
-				print(
-					`Bot '{DiscordClient.Username}#{DiscordClient.Discriminator}' set status to: {discordPresence.Status}`
-				)
-			end)
-
-			Task.wait(20)
-		end
+	DiscordClient:UpdatePresenceAsync(discordPresence):andThen(function()
+		print(
+			`Updated '{DiscordClient.Username}#{DiscordClient.Discriminator}' activity!`
+		)
 	end)
 end)
 
