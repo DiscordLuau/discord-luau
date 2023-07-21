4adfc663bb4a297f1c2ec2b5b6e791011d030d00
AsynchronousMatrix <catbob614@gmail.com>
2023-07-21 23:44:06 +0100

    feature: update exposed classes

--- Source/init.lua
+++ Source/init.lua
@@ -9,5 +9,6 @@ DiscordLuaU.Interface.DiscordClient = require("Classes/DiscordClient")
 DiscordLuaU.Interface.DiscordIntents = require("Classes/DiscordIntents")
 DiscordLuaU.Interface.DiscordPresence = require("Classes/DiscordPresence")
 DiscordLuaU.Interface.DiscordActivity = require("Classes/DiscordActivity")
+DiscordLuaU.Interface.DiscordSettings = require("Classes/DiscordSettings")
 
 return DiscordLuaU.Interface
