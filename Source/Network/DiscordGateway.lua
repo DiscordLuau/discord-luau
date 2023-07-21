a9492c89979cc23d7ef8c6d41887926a2dd8dea8
AsynchronousMatrix <catbob614@gmail.com>
2023-07-21 23:42:01 +0100

    refactor: refactor the Discord API networking to be based around sharding

--- Source/Network/DiscordGateway.lua
+++ Source/Network/DiscordGateway.lua
@@ -1,7 +1,11 @@
 local Promise = require("../Dependencies/Github/Promise")
+
+local Serde = require("@lune/serde")
 local Net = require("@lune/net")
 
-local BASE_DISCORD_APP_URL = "https://discordapp.com"
+local DISCORD_API_VERSION = 10
+
+local BASE_DISCORD_APP_URL = "https://discord.com"
 local BASE_DISCORD_APP_API_PREFIX = "api"
 
 local DiscordGateway = {}
@@ -9,10 +13,8 @@ local DiscordGateway = {}
 DiscordGateway.Type = "DiscordGateway"
 
 DiscordGateway.Internal = {}
+DiscordGateway.Interface = {}
 DiscordGateway.Prototype = {
-	Internal = DiscordGateway.Internal,
-}
-DiscordGateway.Interface = {
 	Internal = DiscordGateway.Internal,
 }
 
@@ -24,32 +26,73 @@ function DiscordGateway.Prototype:GetDiscordAppUri()
 	return `{BASE_DISCORD_APP_URL}/{BASE_DISCORD_APP_API_PREFIX}`
 end
 
-function DiscordGateway.Prototype:GetWebsocketUriAsync()
+function DiscordGateway.Prototype:RequestAsync(api, method)
 	return Promise.new(function(resolve, reject)
-		if self.WebsocketUri then
-			return resolve(self.WebsocketUri)
-		else
-			local networkResponse = Net.request({
-				url = `{BASE_DISCORD_APP_URL}/{BASE_DISCORD_APP_API_PREFIX}/gateway`,
-				method = "GET",
-			})
+		local networkResponse = Net.request({
+			url = `{BASE_DISCORD_APP_URL}/{BASE_DISCORD_APP_API_PREFIX}/v{DISCORD_API_VERSION}/{api}`,
+			method = method,
+			headers = {
+				["authorization"] = `Bot {self.DiscordClient.DiscordToken}`,
+				["content-type"] = "application/json"
+			}
+		})
 
-			if not networkResponse.ok then
-				reject(
-					`Failed to fetch discord gateway: {networkResponse.statusCode} - {networkResponse.statusMessage}`
-				)
+		if not networkResponse.ok then
+			reject(
+				`Failed to fetch discord gateway: {networkResponse.statusCode} - {networkResponse.statusMessage}`
+			)
+		end
+
+		resolve(Serde.decode("json", networkResponse.body))
+	end)
+end
+
+function DiscordGateway.Prototype:GetAsync(api)
+	return Promise.new(function(resolve, reject)
+		local cache = self:GetEndpointCache(api)
+		local cacheValue = cache and cache:Get()
+
+		if cacheValue then
+			resolve(cacheValue)
+
+			return
+		end
+
+		local success, response = self:RequestAsync(api, "GET"):await()
+
+		if success then
+			if cache then
+				cache:Set(response)
 			end
 
-			self.WebsocketUri = Net.jsonDecode(networkResponse.body).url
-
-			resolve(self.WebsocketUri)
+			resolve(response)
+		else
+			reject(response)
 		end
 	end)
 end
 
-function DiscordGateway.Interface.new(websocketUri)
+function DiscordGateway.Prototype:PostAsync(api)
+	return self:RequestAsync(api, "POST")
+end
+
+function DiscordGateway.Prototype:SetEndpointCache(endpoint, cache)
+	self.EndpointCaches[endpoint] = cache
+end
+
+function DiscordGateway.Prototype:GetEndpointCache(endpoint)
+	return self.EndpointCaches[endpoint]
+end
+
+function DiscordGateway.Prototype:GetApiVersion()
+	return DISCORD_API_VERSION
+end
+
+function DiscordGateway.Interface.new(discordClient)
 	return setmetatable({
-		WebsocketUri = websocketUri,
+		DiscordClient = discordClient,
+
+		EndpointCaches = {}
 	}, {
 		__index = DiscordGateway.Prototype,
 		__type = DiscordGateway.Type,
--- Source/Network/DiscordWebsocket.lua
+++ Source/Network/DiscordWebsocket.lua
@@ -1,123 +1,51 @@
-local WebsocketOperationCodes = require("../Enums/WebsocketOperationCodes")
 local WebsocketOperationKeys = require("../Enums/WebsocketOperationKeys")
+local WebsocketOperationCodes = require("../Enums/WebsocketOperationCodes")
 
+local State = require("../Dependencies/Github/State")
 local Promise = require("../Dependencies/Github/Promise")
-local Event = require("../Dependencies/Event")
+local Signal = require("../Dependencies/Github/Signal")
+local Console = require("../Dependencies/Github/Console")
 
-local Observer = require("../Dependencies/Observer")
 local Provider = require("../Dependencies/Provider")
 local Buffer = require("../Dependencies/Buffer")
 
 local Net = require("@lune/net")
 local Serde = require("@lune/serde")
-local Process = require("@lune/process")
 local Task = require("@lune/task")
 
-local ZLIB_SUFFIX = "\120\156\133\84"
+local ZLIB_SUFFIX_START = "\120"
+local ZLIB_SUFFIX_END = "\84"
+
+local SERDE_NULL_USERDATA = Serde.decode("json", "[null]")[1]
 
 local DiscordWebsocket = {}
 
 DiscordWebsocket.Type = "DiscordWebsocket"
 
 DiscordWebsocket.Internal = {}
+DiscordWebsocket.Interface = {}
 DiscordWebsocket.Prototype = {
 	Internal = DiscordWebsocket.Internal,
 }
-DiscordWebsocket.Interface = {
-	Internal = DiscordWebsocket.Internal,
-}
 
-function DiscordWebsocket.Prototype:CreateEventObservers()
-	self.EventProvider:Subscribe(Observer.new("READY", function(dataPacket)
-		self.WebsocketSessionId = dataPacket.session_id
-		self.WebsocketResumeGatewayUrl = dataPacket.resume_gateway_url
-	end))
-end
-
-function DiscordWebsocket.Prototype:CreateOperationalObservers()
-	self.OperationProvider:Subscribe(Observer.new("ClientReconnectAsync", function()
-		self:CancelHeartbeat()
-		self:DisconnectAsync(1005):andThen(function()
-			self:ConnectAsync(true)
-		end)
-	end))
-
-	self.OperationProvider:Subscribe(Observer.new(WebsocketOperationCodes.Dispatch, function(eventName, dataPacket)
-		self.EventProvider:InvokeObservers(eventName, dataPacket)
-	end))
-
-	self.OperationProvider:Subscribe(Observer.new(WebsocketOperationCodes.Heartbeat, function()
-		self:HeartbeatAsync()
-	end))
-
-	self.OperationProvider:Subscribe(Observer.new(WebsocketOperationCodes.Reconnect, function()
-		self.OperationProvider:InvokeObservers("ClientReconnectAsync")
-	end))
-
-	self.OperationProvider:Subscribe(Observer.new(WebsocketOperationCodes.InvalidSession, function(_, dataPacket)
-		if dataPacket then
-			self.OperationProvider:InvokeObservers("ClientReconnectAsync")
-		else
-			error(`Discord session was invalidated, please ensure that the bot's permissions & token is correct!`)
+function DiscordWebsocket.Prototype:RecursiveReplaceTableValue(table, oldValue, newValue)
+	for index, value in table do
+		if value == oldValue then
+			table[index] = newValue
+		elseif type(value) == "table" then
+			self:RecursiveReplaceTableValue(value, oldValue, newValue)
 		end
-	end))
-
-	self.OperationProvider:Subscribe(Observer.new(WebsocketOperationCodes.Hello, function(_, dataPacket)
-		self.WebsocketHeartbeat = (dataPacket.heartbeat_interval / 1000) - 5
-
-		self:CancelHeartbeat()
-		self:StartHeartbeat()
-	end))
-
-	self.OperationProvider:Subscribe(Observer.new(WebsocketOperationCodes.HeartbeatACK, function()
-		self.WebsocketHeartbeatACK = true
-	end))
-end
-
-function DiscordWebsocket.Prototype:HeartbeatAsync()
-	return self:SendAsync({
-		[WebsocketOperationKeys.OperationCode] = WebsocketOperationCodes.Heartbeat,
-		[WebsocketOperationKeys.Data] = self.WebsocketMessageSequence,
-	}):andThen(function(...)
-		self:IdentifyAsync()
-
-		return ...
-	end)
-end
-
-function DiscordWebsocket.Prototype:CancelHeartbeat()
-	if not self.HeartbeatThread then
-		return
 	end
-
-	Task.cancel(self.HeartbeatThread)
-end
-
-function DiscordWebsocket.Prototype:StartHeartbeat()
-	self.HeartbeatThread = Task.spawn(function()
-		Task.wait(math.random())
-
-		self:HeartbeatAsync():await()
-
-		while true do
-			Task.wait(self.WebsocketHeartbeat)
-
-			if not self.WebsocketHeartbeatACK then
-				return self.OperationProvider:InvokeObservers("ClientReconnectAsync")
-			end
-
-			self.WebsocketHeartbeatACK = nil
-			self:HeartbeatAsync():await()
-		end
-	end)
 end
 
 function DiscordWebsocket.Prototype:SendAsync(dataPacket)
 	return Promise.new(function(resolve, reject)
 		local messageSendOperationSuccessful, messageResult =
-			true, self.SocketInstance.send(Serde.encode("json", dataPacket))
+			pcall(self.SocketInstance.send, Serde.encode("json", dataPacket))
 
 		if messageSendOperationSuccessful then
+			self.Reporter:Debug(`Discord Websocket Message Sent: {dataPacket[WebsocketOperationKeys.OperationCode]}`)
+
 			resolve()
 		else
 			reject(messageResult)
@@ -125,96 +53,38 @@ function DiscordWebsocket.Prototype:SendAsync(dataPacket)
 	end)
 end
 
-function DiscordWebsocket.Prototype:IdentifyAsync()
+function DiscordWebsocket.Prototype:ConnectAsync(websocketUrl)
 	return Promise.new(function(resolve, reject)
-		if self.ClientIdentified then
-			return resolve()
-		else
-			self.ClientIdentified = true
+		self.SocketRequestSuccess, self.SocketInstance = pcall(Net.socket, websocketUrl)
+
+		if not self.SocketRequestSuccess then
+			return reject(self.SocketInstance)
 		end
 
-		-- // TO-DO: Implement the following settings:
-		-- sharding
+		self.WebsocketActive:Set(true)
+		self.WebsocketThread = Task.spawn(function()
+			resolve()
 
-		self:SendAsync({
-			[WebsocketOperationKeys.OperationCode] = WebsocketOperationCodes.Identify,
-			[WebsocketOperationKeys.Data] = {
-				["token"] = self.DiscordClient.DiscordToken,
-				["intents"] = self.DiscordClient.DiscordIntents:ToJSONObject(),
-				["properties"] = {
-					["os"] = Process.os,
-					["browser"] = "DiscordLuaU",
-					["device"] = "DiscordLuaU",
-				},
-				["compress"] = true,
-				["large_threshold"] = 250,
-				["shard"] = { 0, 1 },
-			},
-		})
-			:andThen(function()
-				resolve()
-			end)
-			:catch(function(exception)
-				self.ClientIdentified = false
+			while self.WebsocketActive.Value do
+				Task.wait(1)
 
-				reject(exception)
-			end)
-	end)
-end
+				if self.SocketInstance.closeCode then
+					self.WebsocketActive:Set(false)
+					self.OnSocketDead:Fire(self.SocketInstance.closeCode)
+				else
+					local websocketMessage = self.SocketInstance.next()
 
-function DiscordWebsocket.Prototype:ConnectAsync(resumePreviousSession)
-	return Promise.new(function(resolve, reject)
-		self.DiscordClient.Gateway
-			:GetWebsocketUriAsync()
-			:andThen(function(wssUri)
-				self.SocketRequestSuccess, self.SocketInstance =
-					pcall(Net.socket, self.WebsocketResumeGatewayUrl or wssUri)
+					if not websocketMessage then
+						self.WebsocketActive:Set(false)
+						self.OnSocketDead:Fire(self.SocketInstance.closeCode)
 
-				if not self.SocketRequestSuccess then
-					reject(self.SocketInstance)
-				end
-
-				if resumePreviousSession then
-					local success, response = self:SendAsync({
-						[WebsocketOperationKeys.OperationCode] = WebsocketOperationCodes.Resume,
-						[WebsocketOperationKeys.Data] = {
-							["token"] = self.DiscordClient.DiscordToken,
-							["session_id"] = self.WebsocketSessionId,
-							["seq"] = self.WebsocketMessageSequence,
-						},
-					}):await()
-
-					if not success then
-						reject(response)
+						continue
 					end
+
+					self.OnMessageRecv:Fire(websocketMessage)
 				end
-
-				self.WebsocketActive = true
-				self.WebsocketThread = Task.spawn(function()
-					resolve()
-
-					while self.WebsocketActive do
-						Task.wait(1)
-
-						if self.SocketInstance.closeCode then
-							self.WebsocketActive = false
-							self.OnSocketDead:Invoke(self.SocketInstance.closeCode)
-						else
-							local websocketMessage = self.SocketInstance.next()
-
-							if not websocketMessage then
-								self.WebsocketActive = false
-								self.OnSocketDead:Invoke(self.SocketInstance.closeCode)
-							end
-
-							self.OnMessageRecv:Invoke(websocketMessage)
-						end
-					end
-				end)
-			end)
-			:catch(function(exception)
-				reject(exception)
-			end)
+			end
+		end)
 	end)
 end
 
@@ -224,71 +94,83 @@ function DiscordWebsocket.Prototype:DisconnectAsync(closingCode)
 			return resolve()
 		end
 
-		Task.spawn(self.SocketInstance.close, closingCode)
+		self.SocketInstance.close(closingCode)
 
-		self.WebsocketActive = false
-		self.OnSocketDead:Invoke(closingCode)
+		self.WebsocketActive:Set(false)
+		self.OnSocketDead:Fire(closingCode)
 
 		resolve()
 	end)
 end
 
 function DiscordWebsocket.Prototype:ToString()
-	return `{DiscordWebsocket.Type}<"{self.WebsocketActive and "ONLINE" or "OFFLINE"}">`
+	return `{DiscordWebsocket.Type}<"{self.WebsocketActive.Value and "ONLINE" or "OFFLINE"}">`
 end
 
 function DiscordWebsocket.Interface.new(discordClient)
-	local websocket = setmetatable({
-		WebsocketActive = false,
+	local self = setmetatable({
+		WebsocketActive = State.new(false),
 		WebsocketBuffer = Buffer.new(),
 
-		OnMessageRecv = Event.new(),
-		OnSocketDead = Event.new(),
+		OnMessageRecv = Signal.new(),
+		OnSocketDead = Signal.new(),
+
+		Reporter = Console.new("DiscordWebsocket"),
 
 		OperationProvider = Provider.new(),
-		EventProvider = Provider.new(),
 
 		DiscordClient = discordClient,
 	}, {
 		__index = DiscordWebsocket.Prototype,
 		__type = DiscordWebsocket.Type,
-		__tostring = function(self)
-			return self:ToString()
+		__tostring = function(object)
+			return object:ToString()
 		end,
 	})
 
-	websocket.OnSocketDead:Connect(function()
-		print("Websocket has died with error code:", websocket.SocketInstance.closeCode)
+	self.WebsocketActive:Observe(function(_, state)
+		self.Reporter:Debug(`Discord Websocket {(state and "Connected") or "Disconnected"}!`)
 	end)
 
-	websocket.OnMessageRecv:Connect(function(discordPacket)
+	self.OnMessageRecv:Connect(function(discordPacket)
+		
+
 		local isJSON, dataPacket = pcall(Serde.decode, "json", discordPacket)
 
 		if not isJSON then
-			websocket.WebsocketBuffer:Add(discordPacket)
+			self.WebsocketBuffer:Add(discordPacket)
 
-			if string.sub(discordPacket, 1, 4) ~= ZLIB_SUFFIX then
+			if string.sub(discordPacket, 1, 1) ~= ZLIB_SUFFIX_START then
 				return
 			end
 
-			local encodedDiscordPacket = websocket.WebsocketBuffer:Flush()
+			if string.sub(discordPacket, 4, 4) ~= ZLIB_SUFFIX_END then
+				return
+			end
+
+			local encodedDiscordPacket = self.WebsocketBuffer:Flush()
 			local decodedDiscordPacket = Serde.decompress("zlib", encodedDiscordPacket)
 
 			dataPacket = Serde.decode("json", decodedDiscordPacket)
 		end
 
-		websocket.WebsocketMessageSequence = dataPacket[WebsocketOperationKeys.Sequence]
-		websocket.OperationProvider:InvokeObservers(
+		self:RecursiveReplaceTableValue(dataPacket, SERDE_NULL_USERDATA, nil)
+
+		if dataPacket[WebsocketOperationKeys.OperationCode] == WebsocketOperationCodes.Dispatch then
+			self.Reporter:Debug(`Discord Websocket Message Received: {dataPacket[WebsocketOperationKeys.OperationCode]} - {dataPacket[WebsocketOperationKeys.EventName]}`)
+		else
+			self.Reporter:Debug(`Discord Websocket Message Received: {dataPacket[WebsocketOperationKeys.OperationCode]}`)
+		end
+
+		self.OperationProvider:InvokeObservers(
 			dataPacket[WebsocketOperationKeys.OperationCode],
+			dataPacket[WebsocketOperationKeys.Data],
 			dataPacket[WebsocketOperationKeys.EventName],
-			dataPacket[WebsocketOperationKeys.Data]
+			dataPacket[WebsocketOperationKeys.Sequence]
 		)
 	end)
 
-	websocket:CreateEventObservers()
-	websocket:CreateOperationalObservers()
-
-	return websocket
+	return self
 end
 
 return DiscordWebsocket.Interface
