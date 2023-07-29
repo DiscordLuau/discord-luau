a21589a324bc2f8812b547544b27612eb6aaff1e
AsynchronousMatrix <catbob614@gmail.com>
2023-07-29 15:21:52 +0100

    feature: add support for additional http endpoints

--- Source/Enums/DiscordEndpoints.lua
+++ Source/Enums/DiscordEndpoints.lua
@@ -2,5 +2,18 @@ return {
 	BotGateway = "gateway/bot",
 
 	BotGetChannel = "channels/%s",
-	BotCreateMessage = "channels/%s/messages"
+	BotCreateMessage = "channels/%s/messages",
+
+	CreateGlobalApplicationCommand = "applications/%s/commands",
+	GetGlobalApplicationCommands = "applications/%s/commands",
+	GetGlobalApplicationCommand = "applications/%s/commands/%s",
+	DeleteGlobalApplicationCommand = "applications/%s/commands/%s",
+
+	CreateGuildApplicationCommand = "applications/%s/guilds/%s/commands",
+	GetGuildApplicationCommands = "applications/%s/guilds/%s/commands",
+	GetGuildApplicationCommand = "applications/%s/guilds/%s/commands/%s",
+	DeleteGuildApplicationCommand = "applications/%s/guilds/%s/commands/%s",
+
+	CreateInteractionResponse = "interactions/%s/%s/callback",
+	EditOriginalInteractionResponse = "webhooks/%s/%s/messages/@original",
 }
