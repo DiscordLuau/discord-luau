2fa9f6659a3ee9e33ad5963be357989d0b2cae21
AsynchronousMatrix <catbob614@gmail.com>
2023-07-29 15:35:28 +0100

    refactor(multi): improve data flow, implement 'Components' for messages

--- Source/Classes/DiscordMessage.lua
+++ Source/Classes/DiscordMessage.lua
@@ -4,6 +4,9 @@ local DiscordEndpoints = require("../Enums/DiscordEndpoints")
 
 local Promise = require("../Dependencies/Github/Promise")
 
+local DiscordMember = require("Internal/DiscordMember")
+local DiscordUser = require("Internal/DiscordUser")
+
 local DiscordMessage = {}
 
 DiscordMessage.Type = "DiscordMessage"
@@ -38,6 +41,8 @@ end
 
 function DiscordMessage.Prototype:AddEmbed(embedObject)
 	table.insert(self.Embeds, embedObject)
+
+	return self
 end
 
 function DiscordMessage.Prototype:RemoveEmbed(embedObject)
@@ -46,18 +51,42 @@ function DiscordMessage.Prototype:RemoveEmbed(embedObject)
 	if index then
 		table.remove(self.Embeds, index)
 	end
+
+	return self
+end
+
+function DiscordMessage.Prototype:AddComponent(componentObject)
+	table.insert(self.Components, componentObject)
+
+	return self
+end
+
+function DiscordMessage.Prototype:RemoveComponent(componentObject)
+	local index = table.find(self.Components, componentObject)
+
+	if index then
+		table.remove(self.Components, index)
+	end
+
+	return self
 end
 
 function DiscordMessage.Prototype:ToJSONObject()
 	local discordEmbeds = {}
+	local discordComponents = {}
 
 	for index, discordEmbed in self.Embeds do
 		discordEmbeds[index] = discordEmbed:ToJSONObject()
 	end
 
+	for index, discordComponent in self.Components do
+		discordComponents[index] = discordComponent:ToJSONObject()
+	end
+
 	return {
 		content = self.Content,
-		embeds = discordEmbeds
+		embeds = discordEmbeds,
+		components = discordComponents
 	}
 end
 
@@ -65,17 +94,20 @@ function DiscordMessage.Prototype:ToString()
 	return `{DiscordMessage.Type}<{self.Id}>`
 end
 
-function DiscordMessage.Interface.from(discordClient, rawJsonData, discordAuthor)
-	if discordClient:GetFromCache(rawJsonData.id) then
-		return discordClient:GetFromCache(rawJsonData.id)
+function DiscordMessage.Interface.from(discordClient, rawJsonData)
+	if discordClient:GetFromCache(`MESSAGE_{rawJsonData.id}`) then
+		return discordClient:GetFromCache(`MESSAGE_{rawJsonData.id}`)
 	end
 
 	local objectData = Styleguide.new(rawJsonData):PascalCase()
 
 	objectData.DiscordClient = discordClient
-	objectData.Author = discordAuthor
+	objectData.Author = DiscordUser.from(discordClient, rawJsonData.author)
 
-	return discordClient:AddToCache(rawJsonData.id, setmetatable(objectData, {
+	rawJsonData.member.user_id = rawJsonData.author.id
+	objectData.Member = DiscordMember.from(discordClient, rawJsonData.member, rawJsonData.guild_id)
+
+	return discordClient:AddToCache(`MESSAGE_{rawJsonData.id}`, setmetatable(objectData, {
 		__index = DiscordMessage.Prototype,
 		__type = DiscordMessage.Type,
 		__tostring = function(object)
@@ -85,7 +117,11 @@ function DiscordMessage.Interface.from(discordClient, rawJsonData, discordAuthor
 end
 
 function DiscordMessage.Interface.new(messageContent)
-	return setmetatable({ Content = messageContent, Embeds = { } }, {
+	return setmetatable({
+		Content = messageContent,
+		Embeds = { },
+		Components = { }
+	}, {
 		__index = DiscordMessage.Prototype,
 		__type = DiscordMessage.Type,
 		__tostring = function(object)
