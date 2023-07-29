be352239c0d2d2dad9bb6e0215d242f62a344ed2
AsynchronousMatrix <catbob614@gmail.com>
2023-07-29 15:30:50 +0100

    feature: implement support for interacting with global/guild application commands

--- Source/Classes/Internal/DiscordApplication.lua
+++ Source/Classes/Internal/DiscordApplication.lua
@@ -1,3 +1,7 @@
+local Promise = require("../../Dependencies/Github/Promise")
+
+local DiscordEndpoints = require("../../Enums/DiscordEndpoints")
+
 local Styleguide = require("../../Utils/Styleguide")
 
 local DiscordApplication = {}
@@ -10,20 +14,124 @@ DiscordApplication.Prototype = {
 	Internal = DiscordApplication.Internal,
 }
 
+function DiscordApplication.Prototype:GetAllGlobalCommandsAsync()
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:GetAsync(string.format(
+			DiscordEndpoints.GetGlobalApplicationCommands,
+			self.Id
+		)):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:CreateGlobalCommandAsync(commandObject)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:PostAsync(string.format(
+			DiscordEndpoints.CreateGlobalApplicationCommand,
+			self.Id
+		), commandObject:ToJSONObject()):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:GetGlobalCommandAsync(commandId)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:GetAsync(string.format(
+			DiscordEndpoints.GetGlobalApplicationCommand,
+			self.Id, commandId
+		)):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:DeleteGlobalCommandAsync(commandId)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:DeleteAsync(string.format(
+			DiscordEndpoints.DeleteGlobalApplicationCommand,
+			self.Id, commandId
+		)):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:GetAllGuildCommandsAsync(guildId)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:GetAsync(string.format(
+			DiscordEndpoints.GetGuildApplicationCommands,
+			self.Id, guildId
+		)):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:CreateGuildCommandAsync(guildId, commandObject)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:PostAsync(string.format(
+			DiscordEndpoints.GetGuildApplicationCommands,
+			self.Id, guildId
+		), commandObject:ToJSONObject()):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:GetGuildCommandAsync(guildId, commandId)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:GetAsync(string.format(
+			DiscordEndpoints.GetGuildApplicationCommands,
+			self.Id, guildId, commandId
+		)):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
+function DiscordApplication.Prototype:DeleteGuildCommandAsync(guildId, commandId)
+	return Promise.new(function(resolve, reject)
+		self.DiscordClient.Gateway:DeleteAsync(string.format(
+			DiscordEndpoints.GetGuildApplicationCommands,
+			self.Id, guildId, commandId
+		)):andThen(function()
+			resolve()
+		end):catch(function(...)
+			reject(...)
+		end)
+	end)
+end
+
 function DiscordApplication.Prototype:ToString()
 	return `{DiscordApplication.Type}<{self.Id}>`
 end
 
 function DiscordApplication.Interface.from(discordClient, rawJsonData)
-	if discordClient:GetFromCache(rawJsonData.id) then
-		return discordClient:GetFromCache(rawJsonData.id)
+	if discordClient:GetFromCache(`APPLICATION_{rawJsonData.id}`) then
+		return discordClient:GetFromCache(`APPLICATION_{rawJsonData.id}`)
 	end
 
 	local objectData = Styleguide.new(rawJsonData):PascalCase()
 
 	objectData.DiscordClient = discordClient
 
-	return discordClient:AddToCache(rawJsonData.id, setmetatable(objectData, {
+	return discordClient:AddToCache(`APPLICATION_{rawJsonData.id}`, setmetatable(objectData, {
 		__index = DiscordApplication.Prototype,
 		__type = DiscordApplication.Type,
 		__tostring = function(object)
