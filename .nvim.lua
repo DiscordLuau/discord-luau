15c489ee4a3a2684472d9018febe68971238a898
Erica Marigold <hi@devcomp.xyz>
2024-07-14 17:03:14 +0530

    chore(nvim): keep nvim config up-to-date with vscode config

--- .nvim.lua
+++ .nvim.lua
@@ -1,28 +1,39 @@
-require("luau-lsp").config {
-    server = {
-        settings = {
-            ["luau-lsp"] = {
-                inlayHints = {
-                    variableTypes = true,
-                    parameterNames = "all",
-                    typeHintMaxLength = 20,
-                },
-                require = {
-                    mode = "relativeToFile",
-                    directoryAliases = {
-                        ["@lune"] = "~/.lune/.typedefs/0.8.6/",
-                        ["@vendor"] = "./vendor",
-                    },
-                },
-                completion = {
-                    imports = {
-                        enabled = true,
-                    },
-                },
-            },
-        },
-    },
-    platform = {
-        type = "standard",
-    },
-}
+require("luau-lsp").config({
+	server = {
+		settings = {
+			["luau-lsp"] = {
+				inlayHints = {
+					variableTypes = true,
+					functionReturnTypes = true,
+					parameterNames = "all",
+					typeHintMaxLength = 20,
+				},
+				require = {
+					mode = "relativeToFile",
+					directoryAliases = {
+						["@lune"] = "~/.lune/.typedefs/0.8.6/",
+						["@builders"] = "./packages/builders",
+						["@cdn"] = "./packages/cdn/src",
+						["@classes"] = "./packages/classes/src",
+						["@core"] = "./packages/core/src",
+						["@extensions"] = "./packages/extensions/src",
+						["@std-polyfills"] = "./packages/std-polyfills/src",
+						["@api-types"] = "./packages/api-types/src",
+						["@utils"] = "./packages/utils/src",
+						["@vendor"] = "./packages/vendor/src",
+						["@voice"] = "./packages/voice/src",
+						["@websocket"] = "./packages/voice/src",
+					},
+				},
+				completion = {
+					imports = {
+						enabled = true,
+					},
+				},
+			},
+		},
+	},
+	platform = {
+		type = "standard",
+	},
+})
